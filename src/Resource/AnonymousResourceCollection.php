484f4ce00b90f149a5f0a2ffc7dd3a1291bb4bf6
你手捧鲜花 <157215725@qq.com>
2021-07-09 18:41:30 +0800

    update: addHidden logic

--- src/Resource/AnonymousResourceCollection.php
+++ src/Resource/AnonymousResourceCollection.php
@@ -3,8 +3,35 @@
 namespace Lumenx\Resource;
 
 use \Illuminate\Http\Resources\Json\AnonymousResourceCollection as ResourceCollection;
+use Illuminate\Support\Arr;
 
 class AnonymousResourceCollection extends ResourceCollection
 {
-    //
+    protected $hidden = [];
+
+    /**
+     * Transform the resource into a JSON array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return $this->collection->map->toArray($request)->map(function ($item) {
+            return Arr::except($item, $this->hidden);
+        })->all();
+    }
+
+    /**
+     * Add hidden attributes for the resource collection.
+     *
+     * @param $keys
+     */
+    public function addHidden($keys)
+    {
+        $this->hidden = $keys;
+
+        return $this;
+    }
 }
